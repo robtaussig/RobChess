1f68585ff18218cfc6fc6494743a25ca9360cfa7
Rob Taussig <robert.taussig@gmail.com>
2020-10-28 16:06:44 -0400

    Fix manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,17 +17,11 @@ class MyDocument extends Document {
               <meta name='description' content='Best Chess app in the world' />
               <meta name='format-detection' content='telephone=no' />
               <meta name='mobile-web-app-capable' content='yes' />
-              <meta name='msapplication-config' content='/static/icons/browserconfig.xml' />
               <meta name='msapplication-TileColor' content='#2B5797' />
               <meta name='msapplication-tap-highlight' content='no' />
               <meta name='theme-color' content='#000000' />
                         
-              <link rel='apple-touch-icon' sizes='180x180' href='/static/icons/apple-touch-icon.png' />
-              <link rel='icon' type='image/png' sizes='32x32' href='/static/icons/favicon-32x32.png' />
-              <link rel='icon' type='image/png' sizes='16x16' href='/static/icons/favicon-16x16.png' />
-              <link rel='manifest' href='/static/manifest.json' />
-              <link rel='mask-icon' href='/static/icons/safari-pinned-tab.svg' color='#5bbad5' />
-              <link rel='shortcut icon' href='/static/icons/favicon.ico' />
+              <link rel='manifest' href='/manifest.json' />
 
               <meta property='og:type' content='website' />
               <meta property='og:title' content='RobChess' />
