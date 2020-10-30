b2b738c9c6def73401efbe23313b8e9cf08a1ab3
Rob Taussig <robert.taussig@gmail.com>
2020-10-30 17:21:05 -0400

    Add pre-move arrows

--- components/Board/subcomponents/PreMoveOverlay/subcomponents/PreMove/index.tsx
+++ components/Board/subcomponents/PreMoveOverlay/subcomponents/PreMove/index.tsx
@@ -17,12 +17,38 @@ export const PreMove: FC<PreMoveProps> = ({
   parentHeight,
   parentWidth,
 }) => {
+  const fromRow = Math.floor(from / 8);
+  const toRow = Math.floor(to / 8);
+  const fromCol = (from % 8);
+  const toCol = (to % 8);
 
-  console.log(from, to, parentHeight, parentWidth);
+  const halfVerticalUnit = parentHeight / 16;
+  const halfHorizontalUnit = parentWidth / 16;
 
   return (
     <div className={cn(styles.root, className)}>
-
+      <svg height={parentHeight} width={parentWidth}>
+          <defs>
+              <marker
+                id={`markerArrow${from}${to}`}
+                markerWidth={'12'}
+                markerHeight={'12'}
+                refX="10"
+                refY="6"
+                orient="auto"
+              >
+                  <path d="M2,2 L2,11 L10,6 L2,2" style={{ fill: 'white' }} />
+              </marker>
+          </defs>
+          
+          <line
+            style={{ markerEnd: `url(#markerArrow${from}${to})` }}
+            x1={(fromCol * (parentWidth / 8)) + halfHorizontalUnit}
+            y1={(fromRow * (parentHeight / 8)) + halfVerticalUnit}
+            x2={(toCol * (parentWidth / 8)) + halfHorizontalUnit}
+            y2={(toRow * (parentHeight / 8)) + halfVerticalUnit}
+          />
+        </svg>
     </div>
   );
 };
